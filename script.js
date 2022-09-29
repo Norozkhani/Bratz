64e076baeac16a50411635b8eff4e5225610acd3
Norozkhani <madeleine.norozkhanni@gmail.com>
2022-09-29 11:46:16 +0200

    init

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+function yasmin(e) {
+    e.stopPropagation()
+    let myElement = document.createElement('img');
+    document.getElementById('yasminBox').appendChild(myElement);
+    myElement.src = 'yasmin.gif';
+}
+
+let yasminButton = document.getElementById('yasmin');
+yasminButton.addEventListener('click', yasmin);
+
+*/
+  function moveCircle(e) {
+    let circle = document.getElementById('circle');
+    circle.style.top = e.clientY + 'px';
+    circle.style.left = e.clientX + 'px';
+}
+
+window.addEventListener('mousemove', moveCircle);
