302c46ef4ad2107fca9d36e1be80eb93e2e679a0
Norozkhani <madeleine.norozkhanni@gmail.com>
2022-10-07 15:02:13 +0200

    finised changes

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-/*
-function yasmin(e) {
-    e.stopPropagation()
-    let myElement = document.createElement('img');
-    document.getElementById('yasminBox').appendChild(myElement);
-    myElement.src = 'yasmin.gif';
-}
-
-let yasminButton = document.getElementById('yasmin');
-yasminButton.addEventListener('click', yasmin);
-
-
-  function moveCircle(e) {
-    let circle = document.getElementById('circle');
-    circle.style.top = e.clientY + 'px';
-    circle.style.left = e.clientX + 'px';
-}
-
-window.addEventListener('mousemove', moveCircle);
-
-*/
-
 function myFunction() {
   var element = document.body;
   element.classList.toggle("dark-mode");
